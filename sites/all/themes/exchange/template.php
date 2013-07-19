b27a28115631f7e57e6a801fdf03b440bc09af21
hccadmin <dave_rothfarb@dfci.harvard.edu>
2013-07-19 10:41:22 -0400

    Trying to highlight parent menu item when children active

--- sites/all/themes/exchange/template.php
+++ sites/all/themes/exchange/template.php
@@ -127,6 +127,7 @@ function exchange_preprocess_html(&$variables) {
 	drupal_add_html_head($viewport, 'viewport');
 }
 
+
 /**
  * Override or insert variables into the page template for HTML output.
  */
@@ -541,3 +542,27 @@ function exchange_tablesort_indicator($variables) {
 		);
   }
 }
+
+/**
+function phptemplate_menu_item($variables){
+	print_r($variables);
+}
+ * Override menu item to allow highlighting of parent menu items
+function exchange_menu_item($mid, $children = '', $leaf = TRUE) {
+	  return '<li class="'. ($leaf ? 'leaf' : ($children ? 'test' : 'collapsed')) . (menu_in_active_trail($mid) ? ' actual' : '') .'">'. menu_item_link($mid) . $children ."</li>\n";
+}
+ */
+/**
+ * Override menu item to allow highlighting of parent menu items
+function exchange_menu_item($link, $has_children, $menu='', $in_active_trail = FALSE, $extra_class = NULL){
+	$class = ($menu ? 'expanded' : ($has_children ? 'collapsed' : 'leaf'));
+	if(!empty($extra_class)){
+		$class .= ' ' . $extra_class;
+	}
+	if($in_active_trail){
+		$class .= ' active-trail';
+	}
+	return '<li class"' . $class . '">' . $link . $menu . "</li>\n";
+}
+ */
+
